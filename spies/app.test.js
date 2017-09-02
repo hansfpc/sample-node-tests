5faa9e0a94eb1fcc9de42d8c305cf2508ea57df1
Hans Piña <hansfpc@outlook.com>
2017-09-02 17:32:10 -0300

    Update app.test.js

--- spies/app.test.js
+++ spies/app.test.js
@@ -1,20 +1,26 @@
 const expect = require('expect');
 const rewire = require('rewire');
-const app = rewire('./app');
+
+var app = rewire('./app');
 
 describe('App', () => {
-  let db = {
-    saveUser: expect.createSpy(),
+  var db = {
+    saveUser: expect.createSpy()
   };
   app.__set__('db', db);
 
   it('should call the spy correctly', () => {
-    let spy = expect.createSpy();
-    spy('Hans', 25);
-    expect(spy).toHaveBeenCalledWith('Hans', 25);
+    var spy = expect.createSpy();
+    spy('Andrew', 25);
+    expect(spy).toHaveBeenCalledWith('Andrew', 25);
   });
 
-  it('it should call saveUser with user object', () => {
+  it('should call saveUser with user object', () => {
+    var email = 'andrew@example.com';
+    var password = '123abc';
 
+    app.handleSignup(email, password);
+    expect(db.saveUser).toHaveBeenCalledWith({email, password});
   });
+
 });
